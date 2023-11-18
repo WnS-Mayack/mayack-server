e0310f6d211ebb690f442f1893bfebd8ba34976a
kimdonghun <zhfptm12@naver.com>
2023-11-18 14:25:30 +0900

    feat:게시글 생성

--- src/main/java/com/example/mayak/controller/PostController.kt
+++ src/main/java/com/example/mayak/controller/PostController.kt
@@ -1,9 +1,11 @@
 package com.example.mayak.controller
 
+import com.example.mayak.requests.PostRequest
 import com.example.mayak.service.PostService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -26,7 +28,9 @@ class PostController(
     }
 
     @PostMapping
-    fun create() {
-        postService.create()
+    fun create(
+            @RequestBody postRequest: PostRequest
+    ) {
+        postService.create(postRequest)
     }
 }
--- src/main/java/com/example/mayak/service/PostService.kt
+++ src/main/java/com/example/mayak/service/PostService.kt
@@ -1,6 +1,8 @@
 package com.example.mayak.service
 
 import com.example.mayak.Repository.PostRepository
+import com.example.mayak.entity.Post
+import com.example.mayak.requests.PostRequest
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -18,8 +20,15 @@ class PostService(
     }
 
     @Transactional
-    fun create() {
-
+    fun create(request: PostRequest) {
+        val post = Post(
+                description = request.description,
+                imagePath = request.imagePath,
+                price = request.price,
+                region = request.region,
+                title = request.title
+        )
+        postRespository.save(post)
     }
 
 }
