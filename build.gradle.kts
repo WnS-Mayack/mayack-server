c144136501c19347590828f735d51a72766a3f3f
kimdonghun <zhfptm12@naver.com>
2023-11-18 14:45:03 +0900

    kotlin querydsl 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+	//query dsl
+	kotlin("kapt") version "1.7.10"
+
 	id("org.springframework.boot") version "3.1.5"
 	id("io.spring.dependency-management") version "1.1.3"
 	kotlin("jvm") version "1.8.22"
@@ -43,6 +46,12 @@ dependencies {
 
 	//openapi
 	implementation ("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
+
+	// query dsl
+	implementation ("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+	kapt ("com.querydsl:querydsl-apt:5.0.0:jakarta")
+	kapt ("jakarta.annotation:jakarta.annotation-api")
+	kapt ("jakarta.persistence:jakarta.persistence-api")
 }
 
 tasks.withType<KotlinCompile> {
